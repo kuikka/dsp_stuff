d481d620f07f4a5ea84c738ef6b6e1e507286ce6
Juha Kuikka <juha.kuikka@gmail.com>
2019-08-03 12:10:06 -0700

    Refactor calculation for external use

--- goertzel.h
+++ goertzel.h
@@ -5,21 +5,41 @@
 class Goertzel
 {
 public:
+    static void calculate(float sampling_rate,
+        float target_frequency, 
+        size_t block_size,
+        float &sine, float &cosine)
+    {
+        float N = block_size;
+        int k = (int) (0.5f + ((N * target_frequency) / sampling_rate));
+        float omega = (2.0f * M_PI * k) / N;
+        sine = sin(omega);
+        cosine = cos(omega);
+    }
+
     Goertzel(float sampling_rate,
         float target_frequency,
         size_t block_size)
     {
-        float N = block_size;
-        int k = (int) (0.5 + ((N * target_frequency) / sampling_rate));
-        float omega = (2.0f * M_PI * k) / N;
-        sine = sin(omega);
-        cosine = cos(omega);
+        calculate(sampling_rate,
+            target_frequency,
+            block_size,
+            sine,
+            cosine);
+
         coeff = 2.0 * cosine;
 
         reset();
 
-        std::cout << "Goertzel SR=" << sampling_rate << " N=" << block_size << " FREQ=" << target_frequency << "\n";
-        std::cout << "k=" << k << " coeff=" << coeff << "\n";
+    //    std::cout << "Goertzel SR=" << sampling_rate << " N=" << block_size << " FREQ=" << target_frequency << "\n";
+    //    std::cout << "k=" << k << " coeff=" << coeff << "\n";
+    }
+
+    Goertzel(float sine, float cosine)
+        : sine(sine)
+        , cosine(cosine)
+        , coeff(2 * cosine)
+    {
     }
 
     void reset()
